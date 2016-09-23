5c20b8dba019e2e6e1ea2cbf88f8f589b5ec9bf8
Jeferson Inacio Macedo <jeferson.inacio.macedo@gmail.com>
2016-09-23 20:59:48 -0300

    fix start node

--- front-end/app/components/app.component.js
+++ front-end/app/components/app.component.js
@@ -9,26 +9,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var Usuario_1 = require('../models/Usuario');
 var AppComponent = (function () {
     function AppComponent() {
         this.mensagem = "";
         this.error = false;
-        this.usuario = new Usuario_1.Usuario();
     }
     AppComponent.prototype.enviar = function () {
         this.error = false;
-        if (this.usuario.login == "") {
-            this.error = true;
-            this.mensagem = "O campo login é de preenchimento obrigatório";
-        }
-        if (this.usuario.senha == "") {
-            this.error = true;
-            this.mensagem += " \n O campo senha é de preenchimento obrigatório";
-        }
-        if (!this.error) {
-            this.mensagem = "";
-        }
     };
     AppComponent = __decorate([
         core_1.Component({
--- front-end/app/components/app.component.ts
+++ front-end/app/components/app.component.ts
@@ -4,26 +4,11 @@ import {Usuario} from '../models/Usuario';
   selector: 'my-app',
   templateUrl: './view/login.html'
 })
-export class AppComponent { 
-	constructor(){
-		this.usuario = new Usuario();
-	}
+export class AppComponent { 	
 	mensagem :String = "";
 	error: boolean = false;
+	
 	enviar(): void {
 		this.error = false;
-		if(this.usuario.login == ""){
-			this.error = true;
-			this.mensagem = "O campo login é de preenchimento obrigatório";
-		}
-
-		if(this.usuario.senha == ""){
-			this.error = true;
-			this.mensagem += " \n O campo senha é de preenchimento obrigatório";
-		}
-
-		if(!this.error){
-			this.mensagem = "";
-		}
     }
 }
